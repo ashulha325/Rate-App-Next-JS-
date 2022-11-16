f521d6b5ce77a59eb04ebc094edbfe9a353dcbf1
ashulha325 <ashulha325@gmail.com>
2022-11-16 23:35:10 +0200

    smart component Htag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
+import {Htag} from "../components";
 
 export default function Home(): JSX.Element {
-
     return (
-        <div>
-            ssss
-        </div>
+        <>
+            <Htag tag='h1'>Текст</Htag>
+        </>
     );
 }
