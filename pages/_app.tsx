fe396eea909da6cc7ad189204845cbbbade81814
ashulha325 <ashulha325@gmail.com>
2022-11-16 22:42:20 +0200

    refactor code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import type {AppProps} from 'next/app';
+import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) : JSX.Element {
-  return <Component {...pageProps} />;
+export default function App({Component, pageProps}: AppProps): JSX.Element {
+    return (
+        <>
+            <Head>
+                <title>My Top - наш лучший топ!</title>
+                <link key={1} rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
