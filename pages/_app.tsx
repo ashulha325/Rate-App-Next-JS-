34c4a763c563c8a0e837a18d1ab937f6ee8abecb
ashulha325 <ashulha325@gmail.com>
2022-11-16 23:11:30 +0200

    add eslint rule for imports , clear project , add css variables color , add font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,9 @@ export default function App({Component, pageProps}: AppProps): JSX.Element {
             <Head>
                 <title>My Top - наш лучший топ!</title>
                 <link key={1} rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='crossOrigin' />
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700;900&display=swap" rel="stylesheet"/>
             </Head>
             <Component {...pageProps} />
         </>
