e673e87ff78ba9bd509e7303cc0a783822882070
ashulha325 <ashulha325@gmail.com>
2022-11-18 21:36:08 +0200

    footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,6 +1,17 @@
 import { FooterProps } from "./Footer.props";
 import styles from "./Footer.module.css";
-import cb from "classnames";
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <footer {...props}>Footer</footer>;
+import cn from "classnames";
+import { format } from "date-fns";
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <p>OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены</p>
+      <a href={"#"} target={"_blank"}>
+        Пользовательское соглашение
+      </a>
+      <a href={"#"} target={"_blank"}>
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
